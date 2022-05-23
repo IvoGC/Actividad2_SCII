474241f300381639773f9f2927b0ca2b73a3cb76
IvoGC <102243324+IvoGC@users.noreply.github.com>
2022-05-23 14:57:51 -0300

    Add files via upload

---
+++ motor_con_observador_comparacion.m
@@ -0,0 +1,141 @@
+clc; clear ; close all
+%{
+---------------------------------------------------------------------------
+Considerar que no puede medirse la corriente y sólo pueda medirse el ángulo, por lo 
+que debe implementarse un observador. Obtener la simulación en las mismas condiciones que en 
+el punto anterior, y superponer las gráficas para comparar.
+---------------------------------------------------------------------------
+%}
+%este es el sistema con las 3 variables de estado calculadas con el sistema
+%observado
+%DEFINO PARAMETROS
+LAA = 366*10^-6;
+J = 5*10^-9;
+RA = 55.6;
+Bm = 0;
+Ki = 6.49*10^-3;
+Km = 6.53*10^-3;
+
+%DEFINO MATRICES
+%X=[ia ; tita ; w];
+A=[-RA/LAA 0 -Km/LAA  ; 0 0 1 ; Ki/J 0 -Bm/J];
+B=[1/LAA; 0; 0];
+C=[0 1 0];
+D=[0];
+%Dejamos explicitas las matrices del sistema dual qeu es el "observado"
+An=[A zeros(3,1); -C 0];
+Bn=[B ; 0];
+Cn=[1 0];
+Ao=A';
+Bo=C';
+Co=B';
+
+%CALCULO DEL CONTROLADOR K
+%para el calculo del mismo se utiliza el metodo LQR para lo cual definimos
+Q=diag([1 1/800000 1/9000000 10000]); R=0.01;
+K4=lqr(An,Bn,Q,R);
+K=K4(1:3);
+
+K_i=-K4(4);
+
+
+
+Qo=diag([1 1 1/1000]); Ro=1;
+Ko=lqr(Ao,Bo,Qo,Ro)
+%Ko=1e4*[-0.0001 0.7639 3.1623];
+
+
+%implementacion de funciones a usar
+tf=2; dt=1*10^-5; t=0:dt:(tf-dt); periodo=0.6;%[seg]
+torq=1.15*10^-3;
+
+Ref=pi/2*square(2*pi*t/periodo);%funcion de referencia que varia entre pi/2 y -pi/2
+TL=torq/2*square(2*pi*t/periodo)+torq/2;%Funcion torque que varia entre 0 y 1.15*10^-3
+
+%iteracion
+n=round(tf/dt);
+X=zeros(3,n);
+X(1,1)=0; %ia inicial
+X(2,1)=0; %tita inicial
+X(3,1)=0; %w inicial
+psi(1)=0; %psi inicial
+Xcom=zeros(3,n);
+Xcom(1,1)=0; %ia_com inicial
+Xcom(2,1)=0; %tita_com inicial
+Xcom(3,1)=0; %w_com inicial
+psi(1)=0; %psi inicial
+psicom(1)=0;
+Xhat=zeros(3,n);
+Xhat(1,1)=0; %ia_hat inicial
+Xhat(2,1)=0; %tita_hat inicial
+Xhat(3,1)=0; %wr_hat inicial
+U(1)=0;
+Ucom(1)=0;
+
+%DEFINO CONDICIONES INICIALES
+
+for i=1:1:n-1
+    X_a=X(:,i);%[ia ; tita ; w]
+    Xhat_a=Xhat(:,i);%[ia_hat ; tita_hat ; w_hat]
+    Y=C*X_a;
+    Yhat= Co*Xhat_a;
+    err=(Y-Yhat);
+    psi_p=Ref(i)-Y;
+    psi(i+1)=psi(i)+psi_p*dt;
+    U_a=-K(2:3)*X_a(2:3)+K(1)*Xhat_a(1)+K_i*psi(i+1);% U estimando parametros
+    %U_a=-K*Xhat_a+K_i*psi(i+1); % U con las 3 variables de estado estimadas
+    U=[U U_a];
+    
+    
+    Xp_1=-RA/LAA*X_a(1)-Km/LAA*X_a(3)+1/LAA*U_a;  %ia_p
+    Xp_2= X_a(3);                               %tita_p
+    Xp_3=Ki/J*X_a(1)-Bm/J*X_a(3)-1/J*TL(i);%    %W_p
+    Xp_a=[Xp_1 ; Xp_2 ; Xp_3];
+    % Realizamos la integracion de euler y actualizamos matriz X
+    Xf= X_a+ dt*Xp_a; 
+    
+    X(1,i+1)=Xf(1);
+    X(2,i+1)=Xf(2);
+    X(3,i+1)=Xf(3);
+    %Observador
+    
+    Xhat_p=U_a*Bo+Ko'*err+A*Xhat_a;
+    Xhatf=Xhat_a + dt*Xhat_p;
+    Xhat(1,i+1)=Xhatf(1);
+    Xhat(2,i+1)=Xhatf(2);
+    Xhat(3,i+1)=Xhatf(3);
+    
+    %creamos la integracion sin observador a comparar
+    Xcom_a=Xcom(:,i);%[ia_com ; tita_com ; w_com]
+    Ycom=C*Xcom_a;
+    psicom_p=Ref(i)-Ycom;
+    psicom(i+1)=psicom(i)+psicom_p*dt;
+    Ucom_a=-K*Xcom_a+K_i*psicom(i+1);
+    Ucom=[Ucom Ucom_a];
+    
+    Xcom_p_1=-RA/LAA*Xcom_a(1)-Km/LAA*Xcom_a(3)+1/LAA*Ucom_a;  %ia_p
+    Xcom_p_2= Xcom_a(3);                               %tita_p
+    Xcom_p_3=Ki/J*Xcom_a(1)-Bm/J*Xcom_a(3)-1/J*TL(i);%    %W_p
+    Xcom_p_a=[Xcom_p_1 ; Xcom_p_2 ; Xcom_p_3];
+    % Realizamos la integracion de euler y actualizamos matriz X
+    Xcom_f= Xcom_a+ dt*Xcom_p_a; 
+    
+    Xcom(1,i+1)=Xcom_f(1);
+    Xcom(2,i+1)=Xcom_f(2);
+    Xcom(3,i+1)=Xcom_f(3);    
+   
+end
+
+figure
+subplot(2,1,1);
+plot(t,Ref);
+grid on
+hold on
+plot(t,X(2,:),'r');title('angulo tita sin observador pero con integrador');xlabel('tiempo[s]');ylabel('angulo[rad]');legend('REF','var de estado')
+subplot(2,1,2);
+plot(t,Ref);
+grid on
+hold on
+plot(t,X(1,:),'r');title('corriente ia sin observador pero con integrador');xlabel('tiempo[s]');ylabel('angulo[rad]');legend('REF','var de estado')
+
+
--- motor_observador.m
+++ motor_observador.m
@@ -1,14 +1,13 @@
 clc; clear ; close all
 %{
 ---------------------------------------------------------------------------
-Implementar un sistema en variables de estado que controle el ángulo del 
-motor, para consignas de pi/2 y –pi/2 cambiando cada 300 mili segundos y 
-que el TL de 1,15 10-3 aparece sólo para pi/2, para –pi/2 es nulo. 
-Hallar el valor de integración Euler adecuado. El objetivo es mejorar 
-la dinámica del controlador que muestra la Fig. 1. 
+Considerar que no puede medirse la corriente y sólo pueda medirse el ángulo, por lo 
+que debe implementarse un observador. Obtener la simulación en las mismas condiciones que en 
+el punto anterior, y superponer las gráficas para comparar.
 ---------------------------------------------------------------------------
 %}
-%EN ESTE CASO VAMOS A AGREGAR UN INTEGRADOR Y UNA GANANCIA K_I
+%este es el sistema con las 3 variables de estado calculadas con el sistema
+%observado
 %DEFINO PARAMETROS
 LAA = 366*10^-6;
 J = 5*10^-9;
@@ -23,11 +22,24 @@ A=[-RA/LAA 0 -Km/LAA  ; 0 0 1 ; Ki/J 0 -Bm/J];
 B=[1/LAA; 0; 0];
 C=[0 1 0];
 D=[0];
-%pero ahora como vamos a agregar integrador las matrices A y B se ven
-%modificadas
+%Dejamos explicitas las matrices del sistema dual qeu es el "observado"
 An=[A zeros(3,1); -C 0];
 Bn=[B ; 0];
 Cn=[1 0];
+Ao=A';
+Bo=C';
+Co=B';
+
+%CALCULO DEL CONTROLADOR K
+%para el calculo del mismo se utiliza el metodo LQR para lo cual definimos
+Q=diag([1 1/80000 1/20000 10000]); R=0.01;
+K4=lqr(An,Bn,Q,R);
+K=K4(1:3);
+K_i=-K4(4);
+
+Qo=diag([1 1 1/1000]); Ro=1;
+Ko=lqr(Ao,Bo,Qo,Ro)
+
 
 %implementacion de funciones a usar
 tf=2; dt=1*10^-5; t=0:dt:(tf-dt); periodo=0.6;%[seg]
@@ -36,16 +48,6 @@ torq=1.15*10^-3;
 Ref=pi/2*square(2*pi*t/periodo);%funcion de referencia que varia entre pi/2 y -pi/2
 TL=torq/2*square(2*pi*t/periodo)+torq/2;%Funcion torque que varia entre 0 y 1.15*10^-3
 
-%CALCULO DEL CONTROLADOR K
-%para el calculo del mismo se utiliza el metodo LQR para lo cual definimos
-Q=diag([1 1/800000 1/9000000000000000 100000]); R=0.01;
-
-Ka=lqr(An,Bn,Q,R);
-K_i= -Ka(4);
-K=Ka(1:3);
-
-Kn=[K -K_i];
-
 %iteracion
 n=round(tf/dt);
 X=zeros(3,n);
@@ -53,25 +55,42 @@ X(1,1)=0; %ia inicial
 X(2,1)=0; %tita inicial
 X(3,1)=0; %w inicial
 psi(1)=0; %psi inicial
+Xhat=zeros(3,n);
+Xhat(1,1)=0; %ia_hat inicial
+Xhat(2,1)=0; %tita_hat inicial
+Xhat(3,1)=0; %wr_hat inicial
+Up(1)=0;
 %DEFINO CONDICIONES INICIALES
 
 for i=1:1:n-1
     X_a=[X(1,i); X(2,i) ; X(3,i)];%[ia ; tita ; w]
-    psi_p=Ref(i)-C*X_a;
+    Xhat_a=[Xhat(1,i) ; Xhat(2,i) ; Xhat(3,i)];%[ia_hat ; tita_hat ; w_hat]
+    Y=C*X_a;
+    Yhat= Co*Xhat_a;
+    psi_p=Ref(i)-Y;
     psi(i+1)=psi(i)+psi_p*dt;
-    U=-K*X_a+K_i*psi(i+1);
+    U=-K(2:3)*X_a(2:3)+K(1)*Xhat_a(1)+K_i*psi(i+1);% U estimando parametros
+    U=-K*Xhat_a+K_i*psi(i+1); % U con las 3 variables de estado estimadas
+    Up=[Up U];
     
     Xp_1=-RA/LAA*X_a(1)-Km/LAA*X_a(3)+1/LAA*U;  %ia_p
     Xp_2= X_a(3);                               %tita_p
     Xp_3=Ki/J*X_a(1)-Bm/J*X_a(3)-1/J*TL(i);%    %W_p
-    
     Xp_a=[Xp_1 ; Xp_2 ; Xp_3];
-    
-    Xf= X_a+ dt*Xp_a; % Realizamos la integracion de euler y actualizamos matriz X
+    % Realizamos la integracion de euler y actualizamos matriz X
+    Xf= X_a+ dt*Xp_a; 
     
     X(1,i+1)=Xf(1);
     X(2,i+1)=Xf(2);
     X(3,i+1)=Xf(3);
+    %Observador
+    err=(Y-Yhat);
+    Xhat_p=U*B+Ko'*err+A*Xhat_a;
+    Xhatf=Xhat_a + dt*Xhat_p;
+    
+    Xhat(1,i+1)=Xhatf(1);
+    Xhat(2,i+1)=Xhatf(2);
+    Xhat(3,i+1)=Xhatf(3);
     
 end
 %ploteo de entrada con ganancia de prealimentacion U  y perturbacion TL
@@ -84,21 +103,12 @@ end
 % plot(TL);title('Torque de perturbación');xlabel('Tiempo');ylabel('Torque');
 
 
+% figure 
+% plot(t, Up);
+
 figure
-subplot(2,1,1);
 plot(t,Ref);
 grid on
 hold on
-plot(t,X(2,:));title('angulo tita sin observador pero con integrador');xlabel('tiempo[s]');ylabel('angulo[rad]');legend('REF','var de estado')
-subplot(2,1,2);
-plot(t,Ref);
-grid on
-hold on
-plot(t,X(1,:));title('corriente ia sin observador pero con integrador');xlabel('tiempo[s]');ylabel('angulo[rad]');legend('REF','var de estado')
-
-
-
-
-
-
+plot(t,X(2,:));title('angulo tita con observador e integrador');xlabel('tiempo[s]');ylabel('angulo[rad]');legend('REF','var de estado')
 
